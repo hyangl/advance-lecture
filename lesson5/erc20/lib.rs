af5751b8511a677f9b606c56d3ec1ba8a75fcaea
LIUYang <hyangl@163.com>
2021-02-03 15:29:50 +0800

    Add transfer_from, approve, allowance methods of ERC20.

--- lesson5/erc20/lib.rs
+++ lesson5/erc20/lib.rs
@@ -5,43 +5,134 @@ use ink_lang as ink;
 #[ink::contract]
 mod erc20 {
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    use ink_storage::collections::HashMap as StorageHashMap;
+
     #[ink(storage)]
     pub struct Erc20 {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        total_supply: Balance,
+        balances: StorageHashMap<AccountId, Balance>,
+        allowances: StorageHashMap<(AccountId, AccountId), Balance>,
     }
+
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        to: AccountId,
+        #[ink(topic)]
+        value: Balance,
+    }
+
+    #[ink(event)]
+    pub struct Approve {
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        to: AccountId,
+        #[ink(topic)]
+        value: Balance
+    }
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InsufficentBalance,
+
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
 
     impl Erc20 {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(total_supply: Balance) -> Self {
+            let caller = Self::env().caller();
+            let mut balances= StorageHashMap::new();
+            balances.insert(caller, total_supply);
+
+            let instance = Self {
+                total_supply: total_supply,
+                balances: balances,
+                allowances: StorageHashMap::new(),
+            };
+            instance
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            *self.balances.get(&owner).unwrap_or(&0)
         }
+
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            *self.allowances.get(&(owner, spender)).unwrap_or(&0)
+        }
+
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let who = Self::env().caller();
+
+            self.transfer_help(who, to, value)
+        }
+
+        fn transfer_help(&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<()> {
+            let from_balance = self.balance_of(from);
+            if from_balance < value {
+                return Err(Error::InsufficentBalance);
+            }
+            self.balances.insert(from, from_balance - value);
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, to_balance + value);
+
+            Self::env().emit_event(Transfer{
+                from: from,
+                to: to,
+                value: value,
+            });
+
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn transfer_from(&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<()> {
+            let who = Self::env().caller();
+
+            let allowance_balance = self.allowance(from, who);
+            if allowance_balance < value {
+                return Err(Error::InsufficentBalance)
+            }
+            let from_balance = self.balance_of(from);
+            let to_balance = self.balance_of(to);
+            let rt = self.transfer_help(from, to, value);
+            if let Ok(()) = rt {
+                self.allowances.insert((from, to), allowance_balance - value);
+            }
+
+            rt
+        }
+
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
+            let who = Self::env().caller();
+
+            self.allowances.insert((who, spender), value);
+            Self::env().emit_event(Approve {
+                from: who,
+                to: spender,
+                value: value,
+            });
+
+            Ok(())
+        }
+
+
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
@@ -52,20 +143,11 @@ mod erc20 {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// We test if the default constructor does its job.
         #[test]
-        fn default_works() {
-            let erc20 = Erc20::default();
-            assert_eq!(erc20.get(), false);
+        fn create_contract_works() {
+            let erc20 = Erc20::new(1000);
+            assert_eq!(erc20.total_supply(), 1000);
         }
 
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-            let mut erc20 = Erc20::new(false);
-            assert_eq!(erc20.get(), false);
-            erc20.flip();
-            assert_eq!(erc20.get(), true);
-        }
     }
 }
